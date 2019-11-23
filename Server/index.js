6b793be713957027085a8ab054de76201e0785fb
Kent Roth <kentencroth@yahoo.com>
2019-11-23 14:09:40 -0600

    Getting user and ticket by ID

--- Server/index.js
+++ Server/index.js
@@ -20,7 +20,21 @@ app.post('/users', (req, res) => {
 		});
 });
 
-app.post('/Tickets', (req, res) => {
+app.get('/users/:id', (req, res) => {
+	const _id = req.params.id;
+	User.findById({ _id })
+		.then(user => {
+			if (!user) {
+				return res.status(404).send();
+			}
+			res.send(user);
+		})
+		.catch(error => {
+			res.status(500).send;
+		});
+});
+
+app.post('/tickets', (req, res) => {
 	const ticket = new Ticket(req.body);
 
 	ticket
@@ -33,13 +47,28 @@ app.post('/Tickets', (req, res) => {
 		});
 });
 
-app.get('/Tickets', (req, res) => {
+app.get('/tickets', (req, res) => {
 	Ticket.find({})
 		.then(tickets => {
 			res.send(tickets);
 		})
 		.catch(error => {
-			console.log(error);
+			res.status(500).send(error);
+		});
+});
+
+app.get('/tickets/:id', (req, res) => {
+	const _id = req.params.id;
+
+	Ticket.findById(_id)
+		.then(ticket => {
+			if (!ticket) {
+				return res.status(404).send();
+			}
+			res.send(ticket);
+		})
+		.catch(error => {
+			res.status(500).send();
 		});
 });
 
