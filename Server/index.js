4d9d89b4a3e72686ef85ad07d898e0484058f0b4
Kent Roth <kentencroth@yahoo.com>
2019-11-27 12:53:47 -0600

    converted user calls to async await

--- Server/index.js
+++ Server/index.js
@@ -8,30 +8,30 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
 	const user = new User(req.body);
 
-	user.save()
-		.then(() => {
-			res.send(user);
-		})
-		.catch(error => {
-			res.status(400).send(error);
-		});
+	try {
+		await user.save();
+		res.status(201).send(user);
+	} catch (error) {
+		res.status(400).send(error);
+	}
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
 	const _id = req.params.id;
-	User.findById({ _id })
-		.then(user => {
-			if (!user) {
-				return res.status(404).send();
-			}
-			res.send(user);
-		})
-		.catch(error => {
-			res.status(500).send;
-		});
+
+	try {
+		const user = await User.findById({ _id });
+		if (!user) {
+			return res.status(404).send();
+		}
+
+		res.send(user);
+	} catch (error) {
+		res.status(500).send();
+	}
 });
 
 app.post('/tickets', (req, res) => {
