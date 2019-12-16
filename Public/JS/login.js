02e8dcd35457fcdcdaa26d3c832a66ee74350c55
Kent Roth <kentencroth@yahoo.com>
2019-12-16 15:32:12 -0600

    Login fail UI update

--- Public/JS/login.js
+++ Public/JS/login.js
@@ -1,6 +1,24 @@
+const failedLogin = () => {
+	const errorMessage = document.getElementById('errorMessage');
+	errorMessage.setAttribute('class', 'fail');
+
+	const emailError = document.getElementById('email');
+	emailError.setAttribute('class', 'email failInput');
+
+	const passError = document.getElementById('pass');
+	passError.setAttribute('class', 'pass failInput');
+
+	const emailTitle = document.getElementById('emailInput');
+	emailTitle.setAttribute('class', 'emailInput fail');
+
+	const passTitle = document.getElementById('passInput');
+	passTitle.setAttribute('class', 'passInput fail');
+};
+
 function login() {
 	const email = document.getElementById('email').value;
 	const pass = document.getElementById('pass').value;
+
 	axios
 		.post('http://localhost:3000/users/login', {
 			email,
@@ -8,12 +26,11 @@ function login() {
 		})
 		.then(function(response) {
 			localStorage.setItem('authToken', response.data.authToken);
-			console.log(response);
 			if (response.status === 200) {
 				window.location = '/Public/main.html';
 			}
 		})
 		.catch(function(error) {
-			console.log(error);
+			failedLogin();
 		});
 }
