44e23117a0a94797516ed6f8ea1c3e4a129865a7
Kent Roth <kentencroth@yahoo.com>
2020-01-26 12:11:40 -0600

    Resetting password

--- Public/JS/profile.js
+++ Public/JS/profile.js
@@ -19,7 +19,7 @@ logoutAllAccounts = () => {
 			console.log(error);
 		});
 };
-let emial = '';
+let userEmail = '';
 
 gettingProfileInformation = () => {
 	const nameInput = document.getElementById('name');
@@ -29,9 +29,100 @@ gettingProfileInformation = () => {
 		.then(response => {
 			nameInput.setAttribute('value', response.data.name);
 			emailInput.setAttribute('value', response.data.email);
-			email = response.data.email;
+			userEmail = response.data.email;
 		})
 		.catch(error => console.log(error));
+};
+
+saveEditsToProfile = () => {
+	const password = document.getElementById('pass1').value;
+	if (password.length === 0) {
+		changesToProfileNotPassword();
+	} else {
+		passwordUpdate();
+	}
+};
+
+changesToProfileNotPassword = () => {
+	const name = document.getElementById('name').value;
+	const email = document.getElementById('email').value;
+	axios
+		.post('http://localhost:3000/users/login', {
+			email: userEmail,
+			password: currentPass
+		})
+		.then(response => {
+			if (response.status === 200) {
+				console.log('running');
+				axios.patch(
+					'http://localhost:3000/users/me',
+					{
+						name,
+						email,
+						password
+					},
+					config
+				);
+			}
+		})
+		.catch(error => {
+			console.log(error);
+		});
+};
+
+passwordUpdate = () => {
+	const name = document.getElementById('name').value;
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('pass1').value;
+	const currentPass = document.getElementById('currentPass').value;
+	if (password.length === 0) {
+		password = currentPass;
+	}
+
+	axios
+		.post('http://localhost:3000/users/login', {
+			email: userEmail,
+			password: currentPass
+		})
+		.then(response => {
+			if (response.status === 200) {
+				axios.patch(
+					'http://localhost:3000/users/me',
+					{
+						name,
+						email,
+						password
+					},
+					config
+				);
+			}
+		})
+		.then(response => {
+			loginAfterPassChange();
+		})
+		.catch(error => {
+			console.log(error);
+		});
+};
+
+loginAfterPassChange = () => {
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('currentPass').value;
+
+	console.log(email);
+	console.log(password);
+
+	axios
+		.post('http://localhost:3000/users/login', {
+			email,
+			password
+		})
+		.then(response => {
+			localStorage.setItem('authToken', response.data.authToken);
+			if (response.status === 200) {
+				window.location = '/Public/main.html';
+			}
+		});
 };
 
 gettingProfileInformation();
